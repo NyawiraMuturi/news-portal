0369b11cd0d2994ab212cfc4e5b0416f2cd7bfc5
Nyawira Muturi <albinamuturi@gmail.com>
2020-05-12 11:40:22 +0300

    make adjustments to department class

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -5,32 +5,32 @@ import org.sql2o.Connection;
 import java.util.Objects;
 
 public class Department {
-    private String nameofdepartment;
-    private String descrptofdepartment;
+    private String nameOfDepartment;
+    private String descriptionOfDepartment;
     private String numberofemployees;
     private int id;
 
-    public Department(String nameofdepartment,String descrptofdepartment,String nbrEmployees){
-        this.nameofdepartment=nameofdepartment;
-        this.descrptofdepartment=descrptofdepartment;
+    public Department(String nameOfDepartment,String descriptionOfDepartment,String nbrEmployees){
+        this.nameOfDepartment=nameOfDepartment;
+        this.descriptionOfDepartment=descriptionOfDepartment;
         this.numberofemployees=nbrEmployees;
 
     }
 
-    public String getDescrptofdepartment() {
-        return descrptofdepartment;
+    public String getDescriptionOfDepartment() {
+        return descriptionOfDepartment;
     }
 
-    public void setDescrptofdepartment(String descrptofdepartment) {
-        this.descrptofdepartment = descrptofdepartment;
+    public void setDescriptionOfDepartment(String descriptionOfDepartment) {
+        this.descriptionOfDepartment = descriptionOfDepartment;
     }
 
-    public String getNameofdepartment() {
-        return nameofdepartment;
+    public String getNameOfDepartment() {
+        return nameOfDepartment;
     }
 
-    public void setNameofdepartment(String nameofdepartment) {
-        this.nameofdepartment = nameofdepartment;
+    public void setNameOfDepartment(String nameOfDepartment) {
+        this.nameOfDepartment = nameOfDepartment;
     }
 
     public String getNumberofemployees() {
@@ -56,20 +56,20 @@ public class Department {
         Department that = (Department) o;
         return numberofemployees == that.numberofemployees &&
                 id == that.id &&
-                Objects.equals(nameofdepartment, that.nameofdepartment) &&
-                Objects.equals(descrptofdepartment, that.descrptofdepartment);
+                Objects.equals(nameOfDepartment, that.nameOfDepartment) &&
+                Objects.equals(descriptionOfDepartment, that.descriptionOfDepartment);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nameofdepartment, descrptofdepartment, numberofemployees, id);
+        return Objects.hash(nameOfDepartment, descriptionOfDepartment, numberofemployees, id);
     }
     public void save() {
         try(Connection con = models.DB.sql2o.open()) {
             String sql = "INSERT INTO departments (nameofdepartment, descrptofdepartment, numberofemployees) VALUES (:nameofdepartment, :descrptofdepartment, :numberofemployees)";
             this.id = (int) con.createQuery(sql, true)
-                    .addParameter("nameofdepartment", this.nameofdepartment)
-                    .addParameter("descrptofdepartment", this.descrptofdepartment)
+                    .addParameter("nameofdepartment", this.nameOfDepartment)
+                    .addParameter("descrptofdepartment", this.descriptionOfDepartment)
                     .addParameter("numberofemployees", this.numberofemployees)
                     .executeUpdate()
                     .getKey();
