cff54094cbdaaf312850b6d071672639a10fc4d2
Nyawira Muturi <albinamuturi@gmail.com>
2020-05-12 12:15:24 +0300

    imlement CRUD in department interface

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -1,4 +1,25 @@
 package dao;
 
+import models.Department;
+import models.User;
+
+import java.util.List;
+
 public interface DepartmentDao {
+    //read
+    /* static List<Department> getAll();*/
+    Department findById(int id);
+    List<User> getAllUsersOfDepartments(int deparId);
+    List<Department> getAllDepartmentNews(int newsId);
+    //update
+    void update(int id, String deptName, String deptDescription, int employees);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+
+//create
+
+    void add (Department department);
+    void addDepartmentToUsers(Department department, User user);
 }
