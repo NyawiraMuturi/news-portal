426ebd2c8821cb67f9db23c365aceb8115d2cfed
Nyawira Muturi <albinamuturi@gmail.com>
2020-05-12 12:18:41 +0300

    implement CRUD in UserDao interface

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,25 @@
 package dao;
 
+import models.Department;
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
+    //create
+    void add(User users);
+    void addUsersToDepartments(User users, Department department);
+
+    //read
+    /*  List<User> getAll();*/
+    List<Department>getAllDepartmentsForUsers(int id);
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+
+    User findById(int userid);
 }
